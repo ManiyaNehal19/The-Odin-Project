3f857b51154447a19fe4a9d3426aef7383216de6
ManiyaNehal <nehalsheikh140@gmail.com>
2025-01-07 19:14:43 +0500

    brainstorming

---
+++ Project4-TicTacToe/script.js
@@ -0,0 +1,20 @@
+// const calculator = (function () {
+//     const add = (a, b) => a + b;
+//     const sub = (a, b) => a - b;
+//     const mul = (a, b) => a * b;
+//     const div = (a, b) => a / b;
+//     return { add, sub, mul, div };
+//   })();
+//ieffe modal only one instance is created 
+//objects:
+//Gameboard: board array for the game IIFE pattern
+//player 1 
+//player 2
+//gamecontroller turn decision, win conditions
+//displaycontroller (ui part)
+
+//functions:
+// 1) getBoard(), updateBoard(), resetBoard(), isSpotAvailable
+//2) getName, getMarker, 
+//3)startgame playRound checkWinner, switch Turn
+//4) updateBoard, display winner, displaytile, eventlistener
