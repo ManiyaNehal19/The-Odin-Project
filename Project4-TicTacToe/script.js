c62f99e3b48046bcddad0ee00be03251dd4d9c8e
ManiyaNehal <nehalsheikh140@gmail.com>
2025-01-08 10:33:06 +0500

    game works in console

--- Project4-TicTacToe/script.js
+++ Project4-TicTacToe/script.js
@@ -1,20 +1,77 @@
-// const calculator = (function () {
-//     const add = (a, b) => a + b;
-//     const sub = (a, b) => a - b;
-//     const mul = (a, b) => a * b;
-//     const div = (a, b) => a / b;
-//     return { add, sub, mul, div };
-//   })();
-//ieffe modal only one instance is created 
-//objects:
-//Gameboard: board array for the game IIFE pattern
-//player 1 
-//player 2
-//gamecontroller turn decision, win conditions
-//displaycontroller (ui part)
+function Gameboard() {
+    const gameboard = ['', '', '', '', '', '', '', '', ''];
 
-//functions:
-// 1) getBoard(), updateBoard(), resetBoard(), isSpotAvailable
-//2) getName, getMarker, 
-//3)startgame playRound checkWinner, switch Turn
-//4) updateBoard, display winner, displaytile, eventlistener
+    const getBoard = () => gameboard;
+
+    const updateBoard = (index, marker) => {
+        if (isSpotAvailable(index)) {
+            gameboard[index] = marker;
+        }
+    };
+
+    const resetBoard = () => {
+        for (let i = 0; i < gameboard.length; i++) {
+            gameboard[i] = '';
+        }
+    };
+
+    const isSpotAvailable = (index) => gameboard[index] === '';
+
+    return { getBoard, updateBoard, resetBoard, isSpotAvailable };
+}
+
+function Player(name, marker) {
+    const getName = () => name;
+    const getMarker = () => marker;
+
+    return { getName, getMarker };
+}
+
+function gamecontroller() {
+    let players = [Player("player1", 'X'), Player("player 2", 'O')];
+    const board = Gameboard();
+
+    const win = () => {
+        const winConditions = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+        const gameboard = board.getBoard();
+        return winConditions.some(([a, b, c]) => {
+            return gameboard[a] && gameboard[a] === gameboard[b] && gameboard[a] === gameboard[c];
+        });
+    };
+
+    const play = () => {
+        let currentPlayerIndex = 0;
+        console.log("Game START!");
+        for (let i = 0; i < 9; i++) {
+            const currentPlayer = players[currentPlayerIndex];
+            let index = prompt("Write the Index for the array");
+            while (index < 0 || index >= 9 || !board.isSpotAvailable(index)) {
+                index = prompt("Invalid index. Please write the Index for the array");
+            }
+
+            board.updateBoard(index, currentPlayer.getMarker());
+
+            if (win()) {
+                console.log(`${currentPlayer.getName()} WON!`);
+                return;
+            }
+
+            currentPlayerIndex = 1 - currentPlayerIndex;
+        }
+
+        console.log("It was a tie");
+    };
+
+    const startgame = () => {
+        board.resetBoard();
+        play();
+    };
+
+    return { startgame };
+}
+const gametictactoe = gamecontroller();
+gametictactoe.startgame();
