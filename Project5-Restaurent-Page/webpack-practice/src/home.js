823e3c1ed7fd7ed1980c0c712a21916841360aac
ManiyaNehal <nehalsheikh140@gmail.com>
2025-01-25 13:35:57 +0500

    homedone

--- Project5-Restaurent-Page/webpack-practice/src/home.js
+++ Project5-Restaurent-Page/webpack-practice/src/home.js
@@ -1,9 +1,9 @@
+
 import burgerImage from '../Assests/Selbstgemachte Pommes_ So werden sie perfekt _ Stories _ Kitchen Stories.jpg';
 import './style.css';
-
 export function home() {
   const divContent = document.querySelector("#content");
-
+  divContent.innerHTML = ``;
   const textDiv = document.createElement("div");
   textDiv.id = "text";
   textDiv.innerHTML = `
@@ -11,12 +11,44 @@ export function home() {
       Burger Made With Love in Kansas City
     </h1>
   `;
+  Object.assign(textDiv.style, {
+    fontSize:"68px",
+    width: "50%",
+    height: "100%",
+    color: "#fff",
+    
+  });
 
   const imgDiv = document.createElement("div");
   imgDiv.id = "image";
   imgDiv.innerHTML = `
-    <img src="${burgerImage}" alt="Burger-Picture">
+  <img 
+  src="${burgerImage}" 
+  alt="Burger Picture" 
+  style="
+    max-width: 100%; 
+    max-height: 90%; 
+    object-fit: cover; 
+    border-radius: 12px; 
+    transform: rotate(4deg);
+  "
+>
   `;
+  Object.assign(imgDiv.style, {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "50%",
+    height: "100%",
+  });
+
+  Object.assign(divContent.style, {
+    display: "flex",
+    flexDirection: "row",
+    height: "90vh",
+    width: "100%",
+    backgroundColor: "#d94539",
+  });
 
   divContent.appendChild(textDiv);
   divContent.appendChild(imgDiv);
